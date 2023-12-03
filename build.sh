ad6f324387655c43688456df0dfe21a41f955100
itsTPM <26815728+itsTPM@users.noreply.github.com>
2023-12-03 18:57:33 +0300

    fix build.sh paths

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-IMG_FILES="../assets/images/round.png ../assets/images/round_result16.png ../assets/images/round_result32.png ../assets/images/round_result48.png ../assets/images/round_result64.png ../assets/images/round_result128.png ../assets/images/round_result256.png"
-HTML_FILES="../index.html"
-JS_FILES="../assets/js/index.js ../assets/js/content.js"
-CSS_FILES="../assets/css/popup.css ../assets/css/content.css"
+IMG_FILES="./assets/images/round.png ./assets/images/round_result16.png ./assets/images/round_result32.png ./assets/images/round_result48.png ./assets/images/round_result64.png ./assets/images/round_result128.png ./assets/images/round_result256.png"
+HTML_FILES="./index.html"
+JS_FILES="./assets/js/index.js ./assets/js/content.js"
+CSS_FILES="./assets/css/popup.css ./assets/css/content.css"
 EXT_FILES="$JS_FILES $HTML_FILES $IMG_FILES $CSS_FILES"
-CHROME_FILES="$EXT_FILES ../manifest.json"
+CHROME_FILES="$EXT_FILES ./manifest.json"
 FIREFOX_FILES="$EXT_FILES"
 
 NAME="foxford-tools"
