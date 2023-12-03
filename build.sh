f2623def814cbc62620d8b3d792f3be52972fdbb
itsTPM <26815728+itsTPM@users.noreply.github.com>
2023-12-03 19:29:59 +0300

    7z fixxx

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-IMG_FILES="./assets/images/round.png ./assets/images/round_result16.png ./assets/images/round_result32.png ./assets/images/round_result48.png ./assets/images/round_result64.png ./assets/images/round_result128.png ./assets/images/round_result256.png"
-HTML_FILES="./index.html"
-JS_FILES="./assets/js/index.js ./assets/js/content.js"
-CSS_FILES="./assets/css/popup.css ./assets/css/content.css"
+IMG_FILES="assets/images/round.png assets/images/round_result16.png assets/images/round_result32.png assets/images/round_result48.png assets/images/round_result64.png assets/images/round_result128.png assets/images/round_result256.png"
+HTML_FILES="index.html"
+JS_FILES="assets/js/index.js assets/js/content.js"
+CSS_FILES="assets/css/popup.css assets/css/content.css"
 EXT_FILES="$JS_FILES $HTML_FILES $IMG_FILES $CSS_FILES"
-CHROME_FILES="$EXT_FILES ./manifest.json"
+CHROME_FILES="$EXT_FILES manifest.json"
 FIREFOX_FILES="$EXT_FILES"
 
 NAME="foxford-tools"
@@ -15,9 +15,7 @@ rm -f $NAME.crx $NAME.xpi
 
 # Generate Chrome .crx
 7z a -tzip -mx9 -spf $NAME.crx $CHROME_FILES
-7z rn ./foxford-tools.crx .\index.html index.html
-7z rn ./foxford-tools.crx .\assets assets
-7z rn ./foxford-tools.crx .\manifest.json manifest.json
+
 # Generate Firefox .xpi
 #7z a -tzip -mx9 $NAME.xpi $FIREFOX_FILES
 #7z rn $NAME.xpi temp-ff-manifest.json manifest.json
