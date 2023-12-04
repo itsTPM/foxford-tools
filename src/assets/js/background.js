236bf7b714d33b21aa24e0f4998791773ca42ddc
itsTPM <26815728+itsTPM@users.noreply.github.com>
2023-12-05 00:12:58 +0300

    fix background.js for manifest v3

--- src/assets/js/background.js
+++ src/assets/js/background.js
@@ -5,6 +5,11 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                          link.href = '${chrome.runtime.getURL(`themes/${request.theme}.css`)}';
                          document.head.appendChild(link);`;
 
-        chrome.tabs.executeScript({code: cssCode});
+        chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+            chrome.scripting.executeScript({
+                target: {tabId: tabs[0].id},
+                function: new Function(cssCode)
+            });
+        });
     }
 });
