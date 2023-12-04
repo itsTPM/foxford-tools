cb73afa1c571b672aac22beb5cef7390b2bc0071
itsTPM <26815728+itsTPM@users.noreply.github.com>
2023-12-04 19:41:35 +0300

    fix artifact patch in workflow part 6

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 NAME="foxford-tools"
+SRC_DIR="./src"
 
-cd ../src
+rm -f ${NAME}.zip
 
-rm -f ../$NAME.zip
-
-7z a -tzip -mx9 -spf ../$NAME.zip *
+7z a -tzip -mx9 -spf ${NAME}.zip ${SRC_DIR}/*
