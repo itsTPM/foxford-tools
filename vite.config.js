fc709cca2c42d0711475a8d66a99eb381581c6a1
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-26 15:44:13 +0300

    add crxjs for popup HMR
    удобная штука

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json' assert { type: 'json' };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), crx({ manifest })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   base: '',
+  server: {
+    port: 1984,
+    strictPort: true,
+  },
 });
