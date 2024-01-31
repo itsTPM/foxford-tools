3046fc0220728e2dd22113683f6276919979f8a1
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-31 19:25:00 +0300

    small css changes in bookmark element

--- src/components/Bookmarks/Bookmark.vue
+++ src/components/Bookmarks/Bookmark.vue
@@ -8,15 +8,13 @@ const props = defineProps({
 </script>
 
 <template>
-  <div class="flex border-[1px] p-3 gap-0">
-    <div class="flex-grow flex flex-col justify-center">
+  <div class="flex border-[1px] p-3">
+    <div class="flex flex-col justify-center">
       <span class="text-base">{{ bookmark.title }}</span>
       <span class="text-gray-500 text-sm">{{ bookmark.courseName }}</span>
     </div>
-    <div class="flex items-center justify-center">
-      <img :src="bookmark.courseImage" class="w-14" />
+    <div class="flex items-center justify-center flex-shrink-0">
+      <img :alt="bookmark.courseName" :src="bookmark.courseImage" class="w-14" />
     </div>
   </div>
 </template>
-
-<style scoped></style>
