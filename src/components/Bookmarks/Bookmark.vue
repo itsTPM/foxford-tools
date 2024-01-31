fcc6a44ff2b6058cc1c58b9febeb0ca98d319ef2
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-31 19:16:04 +0300

    move bookmark element to another file

---
+++ src/components/Bookmarks/Bookmark.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+  bookmark: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="flex border-[1px] p-3 gap-0">
+    <div class="flex-grow flex flex-col justify-center">
+      <span class="text-base">{{ bookmark.title }}</span>
+      <span class="text-gray-500 text-sm">{{ bookmark.courseName }}</span>
+    </div>
+    <div class="flex items-center justify-center">
+      <img :src="bookmark.courseImage" class="w-14" />
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import Bookmark from '@/components/Bookmarks/Bookmark.vue';
 
 const bookmarks = ref([
   {
@@ -15,14 +16,6 @@ const bookmarks = ref([
   <span v-if="!bookmarks.length" class="text-center text-gray-500 text-sm">Список пуст :(</span>
 
   <div v-for="bookmark in bookmarks" v-else class="flex flex-col gap-3">
-    <div class="flex border-[1px] p-3 gap-0">
-      <div class="flex-grow flex flex-col justify-center">
-        <span class="text-base">{{ bookmark.title }}</span>
-        <span class="text-gray-500 text-sm">{{ bookmark.courseName }}</span>
-      </div>
-      <div class="flex items-center justify-center">
-        <img :src="bookmark.courseImage" class="w-14" />
-      </div>
-    </div>
+    <Bookmark :bookmark />
   </div>
 </template>
