595980435cb7ba3f98f3f5bfe0c63f9fa3d5b299
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-31 18:53:24 +0300

    change classes in headernav cuz of dark theme visual problems

--- src/components/Header/HeaderNav.vue
+++ src/components/Header/HeaderNav.vue
@@ -22,10 +22,7 @@ const routes = [
 <template>
   <nav class="flex gap-3 justify-center">
     <RouterLink v-for="route in routes" :key="route.name" :to="{ name: route.name }">
-      <Button
-        :class="[$route.name === route.name ? ['bg-gray-100', 'border-gray-300'] : '']"
-        size="icon"
-        variant="outline">
+      <Button :class="[$route.name === route.name ? ['bg-active', ''] : '']" size="icon" variant="outline">
         <component :is="route.icon" color="#333" strokeWidth="1.5" />
       </Button>
     </RouterLink>
