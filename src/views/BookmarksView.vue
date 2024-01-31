bd127b874fc11ec071a473ea571159350026e8f2
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-31 16:58:08 +0300

    make bookmarks page placeholder

--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -1,3 +1,22 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
 
-<template></template>
+const bookmarks = ref([{}, {}, {}, {}, {}]);
+</script>
+
+<template>
+  <span v-if="!bookmarks.length" class="text-center text-gray-500 text-sm">Список пуст :(</span>
+  <div v-for="bookmark in bookmarks" v-else class="flex flex-col gap-3">
+    <div class="flex border-[1px] p-3">
+      <div class="flex-grow flex flex-col justify-center">
+        <span class="text-[16px]">Название</span>
+        <span class="text-gray-500 text-[14px]">Название</span>
+      </div>
+      <div>
+        <img
+          class="w-[48px] h-[48px]"
+          src="https://u.foxford.ngcdn.ru/uploads/discipline/svg_image/3/baad170604028da1.svg" />
+      </div>
+    </div>
+  </div>
+</template>
