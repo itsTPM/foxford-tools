f13bb26dcf1a26e0d1943c76c5a2b3e836059f56
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-01-31 18:52:29 +0300

    replaced placeholder in bookmarksview

--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -1,21 +1,27 @@
 <script setup>
 import { ref } from 'vue';
 
-const bookmarks = ref([{}, {}, {}, {}, {}]);
+const bookmarks = ref([
+  {
+    title: 'Пушкин А. С. «Пиковая дама»',
+    courseName: 'Литература',
+    courseImage: 'https://u.foxford.ngcdn.ru/uploads/discipline/svg_image/27/1a8bf269c80f38a1.svg',
+    courseColor: '#6fbb5c',
+  },
+]);
 </script>
 
 <template>
   <span v-if="!bookmarks.length" class="text-center text-gray-500 text-sm">Список пуст :(</span>
+
   <div v-for="bookmark in bookmarks" v-else class="flex flex-col gap-3">
-    <div class="flex border-[1px] p-3">
+    <div class="flex border-[1px] p-3 gap-0">
       <div class="flex-grow flex flex-col justify-center">
-        <span class="text-[16px]">Название</span>
-        <span class="text-gray-500 text-[14px]">Название</span>
+        <span class="text-base">{{ bookmark.title }}</span>
+        <span class="text-gray-500 text-sm">{{ bookmark.courseName }}</span>
       </div>
-      <div>
-        <img
-          class="w-[48px] h-[48px]"
-          src="https://u.foxford.ngcdn.ru/uploads/discipline/svg_image/3/baad170604028da1.svg" />
+      <div class="flex items-center justify-center">
+        <img :src="bookmark.courseImage" class="w-14" />
       </div>
     </div>
   </div>
