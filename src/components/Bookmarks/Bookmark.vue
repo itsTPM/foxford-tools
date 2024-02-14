b6091dbed95bc8e602d085bfc3a6d7beb2cb7f04
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-14 23:50:19 +0300

    feat: show actual bookmarks & remove option

--- src/components/Bookmarks/Bookmark.vue
+++ src/components/Bookmarks/Bookmark.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { IconX } from '@tabler/icons-vue';
+
 const props = defineProps({
   bookmark: {
     type: Object,
@@ -6,19 +8,41 @@ const props = defineProps({
   },
 });
 
+const emits = defineEmits(['bookmarkRemoved']);
+
 const openBookmark = (bookmark) => {
   window.open(bookmark.url, '_blank');
+};
+
+const removeBookmark = (bookmark) => {
+  chrome.storage.sync.get(['readingList'], (result) => {
+    const readingList = result.readingList || [];
+    const newReadingList = readingList.filter((item) => item.url !== bookmark.url);
+    chrome.storage.sync.set({ readingList: newReadingList });
+  });
 };
 </script>
 
 <template>
-  <div class="flex cursor-pointer rounded-md border-[1px] p-3 hover:bg-muted" @click="openBookmark(bookmark)">
+  <div
+    class="relative flex cursor-pointer justify-between rounded-md border-[1px] p-3 hover:bg-muted"
+    @click="openBookmark(bookmark)">
     <div class="flex flex-col justify-center">
       <span class="text-base">{{ bookmark.title }}</span>
       <span class="text-sm text-muted-foreground">{{ bookmark.courseName }}</span>
     </div>
     <div class="flex flex-shrink-0 items-center justify-center">
       <img :alt="bookmark.courseName" :src="bookmark.courseImage" class="w-14" />
+    </div>
+    <div class="absolute right-1 top-1">
+      <div
+        class="text-muted-foreground transition hover:text-primary focus:outline-none"
+        @click.stop="
+          removeBookmark(bookmark);
+          $emit('bookmarkRemoved', bookmark);
+        ">
+        <IconX class="h-5 w-5" />
+      </div>
     </div>
   </div>
 </template>
--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -1,22 +1,30 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import Bookmark from '@/components/Bookmarks/Bookmark.vue';
 
-const bookmarks = ref([
-  {
-    title: 'Пушкин А. С. «Пиковая дама»',
-    url: 'https://google.com',
-    courseName: 'Литература',
-    courseImage: 'https://u.foxford.ngcdn.ru/uploads/discipline/svg_image/27/1a8bf269c80f38a1.svg',
-    courseColor: '#6fbb5c',
-  },
-]);
+const bookmarks = ref([]);
+
+onMounted(() => {
+  const bookmarksList = new Promise((resolve) => {
+    chrome.storage.sync.get(['readingList'], (result) => {
+      resolve(result.readingList || []);
+    });
+  });
+
+  bookmarksList.then((result) => {
+    bookmarks.value = result;
+  });
+});
+
+const onBookmarkRemoved = (bookmark) => {
+  bookmarks.value = bookmarks.value.filter((item) => item.url !== bookmark.url);
+};
 </script>
 
 <template>
   <span v-if="!bookmarks.length" class="text-center text-sm text-muted-foreground">Список пуст :(</span>
 
   <div v-for="bookmark in bookmarks" v-else class="flex flex-col gap-3">
-    <Bookmark :bookmark />
+    <Bookmark :bookmark @bookmarkRemoved="onBookmarkRemoved" />
   </div>
 </template>
