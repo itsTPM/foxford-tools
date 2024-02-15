7e53b7c1831c6a6a5168ceb8602fa62b9659d5b0
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-15 18:06:37 +0300

    feat: remove hardcoded version, now import it from manifest

--- src/components/Footer/FooterMeta.vue
+++ src/components/Footer/FooterMeta.vue
@@ -1,10 +1,11 @@
 <script setup>
 import { IconVersions } from '@tabler/icons-vue';
+import manifest from '../../../manifest.json';
 </script>
 
 <template>
   <div class="flex items-center justify-center gap-1 text-sm text-muted-foreground">
     <IconVersions strokeWidth="1.5"></IconVersions>
-    <span>версия 1.2-vue</span>
+    <span>версия {{ manifest.version_name }}</span>
   </div>
 </template>
