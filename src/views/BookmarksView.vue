3da718522a8e42a72c2778fd3a20604057f8ad8d
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-26 22:03:56 +0300

    feat: new handler for empty bookmark list

--- src/views/BookmarksView.vue
+++ src/views/BookmarksView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, ref } from 'vue';
 import Bookmark from '@/components/Bookmarks/Bookmark.vue';
+import { IconMoodPuzzled } from '@tabler/icons-vue';
 
 const bookmarks = ref([]);
 
@@ -22,9 +23,19 @@ const onBookmarkRemoved = (bookmark) => {
 </script>
 
 <template>
-  <span v-if="!bookmarks.length" class="text-center text-sm text-muted-foreground">Список пуст :(</span>
-
-  <div v-for="bookmark in bookmarks" v-else class="flex flex-col gap-3">
+  <div v-for="bookmark in bookmarks" v-if="bookmarks.length" class="flex flex-col gap-3">
     <Bookmark :bookmark @bookmarkRemoved="onBookmarkRemoved" />
+  </div>
+
+  <div v-else>
+    <div class="flex justify-between rounded-md border p-3">
+      <div class="flex flex-col justify-center">
+        <span class="text-base">А где?</span>
+        <span class="text-sm text-muted-foreground">закладок пока нет</span>
+      </div>
+      <div class="relative flex items-center text-muted">
+        <IconMoodPuzzled size="48" strokeWidth="1.75" />
+      </div>
+    </div>
   </div>
 </template>
