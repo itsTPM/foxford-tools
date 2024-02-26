8ed66461eece987f70e9f4a5fe11f58d9b1b420d
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-26 21:31:34 +0300

    feat: add changelog link to footer

--- src/components/Footer/FooterMeta.vue
+++ src/components/Footer/FooterMeta.vue
@@ -8,6 +8,14 @@ const versionName = manifest.version_name;
 <template>
   <div class="flex items-center justify-center gap-1 text-sm text-muted-foreground">
     <IconVersions strokeWidth="1.5"></IconVersions>
-    <span>версия {{ versionName }}</span>
+    <div class="flex flex-col leading-[0.8]">
+      <span>версия {{ versionName }}</span>
+      <a
+        class="text-xs text-muted-foreground/50 transition hover:text-secondary-foreground/75"
+        href="https://fox.itstpm.tech/changelog"
+        target="_blank">
+        что нового?
+      </a>
+    </div>
   </div>
 </template>
