1bc5ca6db9655a25aa3950dad6155c8441b592c0
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-02-26 13:36:58 +0300

    feat: remove flex container & set text-center for router title component

--- src/components/RouterTitle.vue
+++ src/components/RouterTitle.vue
@@ -1,7 +1,5 @@
 <script setup></script>
 
 <template>
-  <div class="flex items-center justify-center">
-    <span class="text-base font-medium">{{ this.$route.meta.title }}</span>
-  </div>
+  <span class="text-center text-base font-medium">{{ this.$route.meta.title }}</span>
 </template>
