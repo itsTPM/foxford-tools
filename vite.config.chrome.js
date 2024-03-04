1353494fbd51576feab247ec1446eaaa7dfbdc34
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-03-04 22:16:11 +0300

    build: make firefox & chrome building configs, add to package scripts

---
+++ vite.config.chrome.js
@@ -0,0 +1,36 @@
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { crx } from '@crxjs/vite-plugin';
+import manifest from './manifest.json' assert { type: 'json' };
+
+import tailwind from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    crx({
+      manifest,
+      ...{
+        browser: 'chrome',
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  base: '',
+  server: {
+    port: 1984,
+    strictPort: true,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+});
