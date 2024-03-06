40629e012546186a6d23dd8c601f357e60e7b687
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-03-06 23:47:47 +0300

    chore: cleanup configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,12 @@ module.exports = {
   darkMode: ['class'],
   safelist: ['dark'],
 
-  content: [
-    './pages/**/*.{js,jsx,vue}',
-    './components/**/*.{js,jsx,vue}',
-    './app/**/*.{js,jsx,vue}',
-    './src/**/*.{js,jsx,vue}',
-  ],
+  content: ['./src/**/*.{js,jsx,vue}'],
 
   theme: {
     container: {
       center: true,
       padding: '2rem',
-      screens: {
-        '2xl': '1400px',
-      },
     },
     extend: {
       colors: {
