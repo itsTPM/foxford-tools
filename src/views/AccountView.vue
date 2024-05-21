337c7a65ab662ace8d6f0461583cddf1ae8084cb
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-05-21 22:17:21 +0300

    feat: check if data is missing

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -7,7 +7,7 @@ const profileData = ref({
   full_name: '',
   avatar_url: '',
   created_at: null,
-  bonus_amount: 0,
+  bonus_amount: null,
 });
 
 const levelData = ref({
@@ -46,37 +46,40 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex items-center gap-5 rounded-md border p-3">
-    <img :src="profileData.avatar_url" alt="Аватар пользователя" class="h-16 w-16 rounded-lg object-contain" />
-    <div class="flex flex-col">
-      <h1 class="text-lg font-medium">{{ profileData.full_name }}</h1>
-      <p class="text-muted-foreground">
-        создан:
-        {{ new Date(profileData.created_at).toLocaleDateString('ru-RU') }}
-      </p>
+  <template v-if="profileData.full_name && profileData.bonus_amount >= 0 && levelData.total_xp">
+    <div class="flex items-center gap-5 rounded-md border p-3">
+      <img :src="profileData.avatar_url" alt="Аватар пользователя" class="h-16 w-16 rounded-lg object-contain" />
+      <div class="flex flex-col">
+        <h1 class="text-lg font-medium">{{ profileData.full_name }}</h1>
+        <p class="text-muted-foreground">
+          создан:
+          {{ new Date(profileData.created_at).toLocaleDateString('ru-RU') }}
+        </p>
+      </div>
     </div>
-  </div>
 
-  <div class="flex justify-between rounded-md border p-3">
-    <div class="flex flex-col justify-center">
-      <span class="text-base">{{ profileData.bonus_amount }} фоксиков</span>
-      <span class="text-sm text-muted-foreground">у вас на счету</span>
-    </div>
-    <div class="flex items-center text-muted">
-      <IconCoins :size="48" strokeWidth="1.75" />
-    </div>
-  </div>
-
-  <div class="flex flex-col">
-    <div class="flex justify-between rounded-md rounded-b-none border p-3">
+    <div class="flex justify-between rounded-md border p-3">
       <div class="flex flex-col justify-center">
-        <span class="text-base">{{ levelData.gained_xp }} из {{ levelData.available_xp }} XP</span>
-        <span class="text-sm text-muted-foreground">до следующего уровня</span>
+        <span class="text-base">{{ profileData.bonus_amount }} фоксиков</span>
+        <span class="text-sm text-muted-foreground">у вас на счету</span>
       </div>
       <div class="flex items-center text-muted">
-        <IconArrowBadgeUp :size="48" strokeWidth="1.75" />
+        <IconCoins :size="48" strokeWidth="1.75" />
       </div>
     </div>
-    <Progress :max="levelData.available_xp" v-model="levelData.gained_xp" class="h-1 rounded-t-none" />
-  </div>
+
+    <div class="flex flex-col">
+      <div class="flex justify-between rounded-md rounded-b-none border p-3">
+        <div class="flex flex-col justify-center">
+          <span class="text-base">{{ levelData.gained_xp }} из {{ levelData.available_xp }} XP</span>
+          <span class="text-sm text-muted-foreground">до следующего уровня</span>
+        </div>
+        <div class="flex items-center text-muted">
+          <IconArrowBadgeUp :size="48" strokeWidth="1.75" />
+        </div>
+      </div>
+      <Progress :max="levelData.available_xp" v-model="levelData.gained_xp" class="h-1 rounded-t-none" />
+    </div>
+  </template>
+  <template v-else>Данные не успели загрузиться, или возникла ошибка при их получении..</template>
 </template>
