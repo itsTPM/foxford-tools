3667522865c1a29c9e1b6eec28d7972ada762d86
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-10-28 12:59:26 +0300

    chore: do not use version_name anymore

--- src/components/Footer/FooterMeta.vue
+++ src/components/Footer/FooterMeta.vue
@@ -2,14 +2,14 @@
 import { IconVersions } from '@tabler/icons-vue';
 import manifest from '../../../manifest.json';
 
-const versionName = manifest.version_name;
+const version = manifest.version;
 </script>
 
 <template>
   <div class="flex items-center justify-center gap-1 text-sm text-muted-foreground">
     <IconVersions stroke-width="1.5" class="w-6" aria-hidden="true"></IconVersions>
     <div class="flex flex-col leading-[0.8]">
-      <p>версия {{ versionName }}</p>
+      <p>версия {{ version }}</p>
       <a
         class="text-xs text-muted-foreground/50 transition hover:text-secondary-foreground/75"
         href="https://github.com/itsTPM/foxford-tools/releases"
