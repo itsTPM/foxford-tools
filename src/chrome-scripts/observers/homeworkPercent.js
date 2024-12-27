16321069df5c05e3e79df784fb2d399edfcd1fc1
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-12-27 23:22:39 +0300

    refactor(homeworkPercent): rewrite entire file

--- src/chrome-scripts/observers/homeworkPercent.js
+++ src/chrome-scripts/observers/homeworkPercent.js
@@ -1,64 +1,119 @@
 import { Observer, PercentElement, Request } from '../classes';
 
-async function calculateHomeworkProgress(element) {
-  let tasksPercent = 0;
-  let tasksCount = 0;
-  let totalTasksCount = 0;
-  // следующие 3 строчки - выцепляем id домашки из href кнопки, потом добавляем его в ссылку на api
-  const homeworkLink = element.parentNode.parentNode.parentNode.parentNode.parentNode.href; // это надо будет заменить
-  const homeworkId = homeworkLink?.match(/[0-9]+/g);
-
-  // trainings - проверочные, на них процентов нет
-  if (!homeworkId || homeworkLink.includes('trainings')) {
-    return;
-  }
-
-  const request = new Request({ url: `lessons/${homeworkId}/tasks`, cacheCallback });
-  const tasksJson = await request.make();
-
-  const statusValues = {
-    solved: 1,
-    partially: 0.5,
-    failed: 0,
-  };
-
-  const ignoredStatuses = ['started', 'not_started', 'hinted', 'in_queue', 'unavailable'];
-
-  if (Array.isArray(tasksJson)) {
-    tasksJson.forEach(({ status }) => {
-      totalTasksCount++;
-
-      if (!ignoredStatuses.includes(status)) {
-        tasksPercent += statusValues[status];
-        tasksCount++;
-      }
-    });
-  }
-
-  const homeworkPercent = Math.round((tasksPercent / tasksCount) * 100);
-  const percentElement = new PercentElement({
-    percent: homeworkPercent,
-    parent: element,
-    insertMethod: 'after',
-  });
-  percentElement.classList.add('homeworkPercent');
-
-  if (homeworkPercent === 100 && totalTasksCount === tasksCount) {
-    percentElement.classList.add('percent-legendary');
-  }
-}
+const IGNORED_STATUSES = ['started', 'not_started', 'hinted', 'in_queue', 'unavailable'];
+const SOLVED_STATUSES = ['solved', 'partially', 'failed'];
+const SOLVED_STATUSES_RATE = {
+  solved: 1,
+  partially: 0.5,
+  failed: 0,
+};
 
 export default function createHomeworkObserver() {
   const observer = new Observer({
     targetElementSelector: '#joyrideHomeworkBtn',
-    createdElementSelector: '.homeworkPercent',
+    createdElementSelector: '#homeworkPercent',
     urlPart: 'courses',
-    callback: calculateHomeworkProgress,
+    callback: observerCallback,
   });
 
   return observer;
 }
 
+async function observerCallback(element) {
+  const homeworkLink = getHomeworkLink(element);
+  const homeworkId = getHomeworkId(homeworkLink);
+
+  const isShouldReturn = checkIsShouldReturn(homeworkId, homeworkLink);
+
+  if (isShouldReturn) {
+    return;
+  }
+
+  const tasks = await getTasks(homeworkId);
+  const { percent, totalTasksCount, solvedTasksCount } = calculatePercent(tasks);
+
+  const percentElement = createPercentElement(percent, element);
+
+  const isShouldUseLegendary = checkIsShouldUseLegendary({ percent, totalTasksCount, solvedTasksCount });
+
+  if (isShouldUseLegendary) {
+    useLegendary(percentElement);
+  }
+}
+
+function getHomeworkId(homeworkLink) {
+  return homeworkLink?.match(/[0-9]+/g);
+}
+
+function checkIsShouldReturn(homeworkId, homeworkLink) {
+  const falsyCases = [!homeworkId || homeworkLink.includes('trainings')];
+
+  return falsyCases.some((falsyCase) => falsyCase);
+}
+
+async function getTasks(homeworkId) {
+  const request = new Request({ url: `lessons/${homeworkId}/tasks`, cacheCallback });
+  const tasks = await request.make();
+
+  return tasks;
+}
+
+function calculatePercent(tasks) {
+  if (!Array.isArray(tasks)) {
+    return;
+  }
+
+  let totalTasksCount = 0;
+  let solvedTasksCount = 0;
+  let solvedTasksRate = 0;
+
+  for (const { status } of tasks) {
+    totalTasksCount++;
+
+    if (!IGNORED_STATUSES.includes(status)) {
+      solvedTasksRate += SOLVED_STATUSES_RATE[status];
+      solvedTasksCount++;
+    }
+  }
+
+  const percent = Math.round((solvedTasksRate / solvedTasksCount) * 100);
+
+  return {
+    percent,
+    totalTasksCount,
+    solvedTasksCount,
+  };
+}
+
+function createPercentElement(percent, parent) {
+  const percentElement = new PercentElement({
+    percent,
+    parent,
+    insertMethod: 'after',
+  });
+
+  setPercentElementAttributes(percentElement);
+
+  return percentElement;
+}
+
 function cacheCallback(data) {
-  return data.every(({ status }) => status === 'solved' || status === 'partially' || status === 'failed');
+  return data.every(({ status }) => SOLVED_STATUSES.includes(status));
+}
+
+function getHomeworkLink(element) {
+  return element.parentNode.parentNode.parentNode.parentNode.parentNode.href;
+}
+
+function setPercentElementAttributes(percentElement) {
+  percentElement.id = 'homeworkPercent';
+  percentElement.classList.add('homeworkPercent');
+}
+
+function checkIsShouldUseLegendary({ percent, totalTasksCount, solvedTasksCount }) {
+  return percent === 100 && totalTasksCount === solvedTasksCount;
+}
+
+function useLegendary(element) {
+  element.classList.add('percent-legendary');
 }
