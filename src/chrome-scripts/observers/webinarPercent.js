497b74a45c5f2ad5938a406cefb379161c8994bd
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-12-28 00:19:33 +0300

    refactor(webinarPercent)

--- src/chrome-scripts/observers/webinarPercent.js
+++ src/chrome-scripts/observers/webinarPercent.js
@@ -1,30 +1,60 @@
 import { PercentElement, Observer } from '../classes';
 
-function calculateWebinarProgress(element) {
-  // У элемента текст че-то типа "60 из 120 XP", достаем 60 и 120
-  const xpElementText = element.lastChild.lastChild.lastChild.lastChild.firstChild.lastChild.textContent;
-
-  const xpNumbers = xpElementText.match(/[0-9]+/g);
-  const [gainedXp, maxXp] = xpNumbers.map((number) => parseInt(number));
-
-  // null ставится чтобы createPercentElement использовал текст "не начато" заместо 0%
-  // так как из-за подсчета по числам xp а не по api тут даже проваленная задача даст 10xp и 0% быть не может
-  const webinarPercent = +Math.round((gainedXp / maxXp) * 100) || null;
-
-  new PercentElement({
-    percent: webinarPercent,
-    parent: element.lastChild.lastChild.lastChild.lastChild,
-    insertMethod: 'before',
-  }).classList.add('webinarPercent');
-}
-
 export default function createWebinarObserver() {
   const observer = new Observer({
     targetElementSelector: '#joyrideLessonBtn',
     createdElementSelector: '.webinarPercent',
     urlPart: 'courses',
-    callback: calculateWebinarProgress,
+    callback: observerCallback,
   });
 
   return observer;
+}
+
+function observerCallback(element) {
+  const xpElement = getXpElement(element);
+  const xpElementText = getXpElementText(xpElement);
+  const extractedXp = extractXp(xpElementText);
+  const [gainedXp, maxXp] = parseXp(extractedXp);
+  const percent = calculatePercent(gainedXp, maxXp);
+
+  createPercentElement(percent, element);
+}
+
+function getXpElement(element) {
+  return element.lastChild.lastChild.lastChild.lastChild.firstChild.lastChild;
+}
+
+function getXpElementText(xpElement) {
+  return xpElement.textContent;
+}
+
+function extractXp(xpElementText) {
+  return xpElementText.match(/[0-9]+/g);
+}
+
+function parseXp(extractedXp) {
+  return extractedXp.map((xp) => parseInt(xp));
+}
+
+function calculatePercent(gainedXp, maxXp) {
+  // null ставится чтобы использовался текст "не начато" заместо 0%, так как из-за подсчета по числам xp а не по api тут даже проваленная задача даст 10xp и 0% быть не может
+  return +Math.round((gainedXp / maxXp) * 100) || null;
+}
+
+function createPercentElement(percent, element) {
+  const percentElement = new PercentElement({
+    percent,
+    parent: element.lastChild.lastChild.lastChild.lastChild,
+    insertMethod: 'before',
+  });
+
+  setPercentElementAttributes(percentElement);
+
+  return percentElement;
+}
+
+function setPercentElementAttributes(percentElement) {
+  percentElement.id = 'webinarPercent';
+  percentElement.classList.add('webinarPercent');
 }
