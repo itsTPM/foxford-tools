8cf7a884fb16600f5f33979804e4417c8835c8c5
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-12-28 00:30:41 +0300

    refactor(readingTime)

--- src/chrome-scripts/observers/readingTime.js
+++ src/chrome-scripts/observers/readingTime.js
@@ -1,32 +1,59 @@
 import { Element, Observer } from '../classes';
 
-function calculateReadingTime(element) {
-  const conspectContent = element.textContent;
-  const wordCount = [...conspectContent.matchAll(/[^\s]+/g)].length;
-  const readingTime = Math.round(wordCount / 150);
-  const badgeWrapper = new Element({
-    tag: 'div',
-    properties: { className: 'badgeWrapper' },
-    parent: element.parentNode,
-    insertMethod: 'prepend',
-  });
-
-  new Element({
-    tag: 'span',
-    properties: {
-      textContent: readingTime > 0 ? `~${readingTime} мин. чтения` : `меньше минуты чтения`,
-    },
-    parent: badgeWrapper,
-  });
-}
-
 export default function createReadingTimeObserver() {
   const observer = new Observer({
     targetElementSelector: '#wikiThemeContent',
-    createdElementSelector: '.badgeWrapper',
+    createdElementSelector: '#readingTime',
     urlPart: 'conspects',
-    callback: calculateReadingTime,
+    callback: observerCallback,
   });
 
   return observer;
+}
+
+function observerCallback(element) {
+  const conspectText = getConspectText(element);
+  const wordCount = calculateWordCount(conspectText);
+  const readingTime = calculateReadingTime(wordCount);
+  const readingTimeElementText = calculateReadingTimeElementText(readingTime);
+
+  createReadingTimeElement({
+    textContent: readingTimeElementText,
+    element,
+  });
+}
+
+function getConspectText(element) {
+  return element.textContent;
+}
+
+function calculateWordCount(text) {
+  return [...text.matchAll(/[^\s]+/g)].length;
+}
+
+function calculateReadingTime(wordCount) {
+  const WORDS_PER_MINUTE = 150;
+
+  return Math.round(wordCount / WORDS_PER_MINUTE);
+}
+
+function calculateReadingTimeElementText(readingTime) {
+  if (readingTime > 0) {
+    return `~${readingTime} мин. чтения`;
+  }
+
+  return `меньше минуты чтения`;
+}
+
+function createReadingTimeElement({ textContent, element }) {
+  new Element({
+    tag: 'div',
+    properties: {
+      textContent,
+      className: 'readingTime',
+      id: 'readingTime',
+    },
+    parent: element.parentNode,
+    insertMethod: 'prepend',
+  });
 }
