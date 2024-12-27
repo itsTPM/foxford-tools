ab7793540051c42ed5d05fba1babf1a08d933aff
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-12-28 00:45:20 +0300

    refactor(searchButton)

--- src/chrome-scripts/observers/searchButton.js
+++ src/chrome-scripts/observers/searchButton.js
@@ -1,42 +1,76 @@
 import { Element, Observer, Request } from '../classes';
 import googleIcon from '@/chrome-scripts/assets/google-logo.svg?url';
 
-function addSearchButton(element) {
-  const searchElement = new Element({
-    tag: 'div',
-    properties: { className: 'searchButton' },
+export default function createSearchButtonObserver() {
+  const observer = new Observer({
+    targetElementSelector: 'div[class*="theory__Root"]',
+    createdElementSelector: '#searchButton',
+    urlPart: 'courses',
+    callback: observerCallback,
+  });
+
+  return observer;
+}
+
+async function observerCallback(element) {
+  const searchButtonElement = createSearchButtonElement(element);
+
+  searchButtonElement.addEventListener('click', await searchButtonElementOnClick);
+}
+
+function createSearchButtonElement(element) {
+  const searchButtonElement = new Element({
+    tag: 'button',
+    properties: {
+      className: 'searchButton',
+      id: 'searchButton',
+      ariaLabel: 'Искать теорию по теме в Google',
+    },
     parent: element,
     insertMethod: 'append',
   });
 
-  const img = new Element({
-    tag: 'img',
-    properties: { src: googleIcon },
-    parent: searchElement,
-  });
+  createSearchButtonIconElement(searchButtonElement);
 
-  const searchGoogle = async (e) => {
-    e.preventDefault();
-    const [courseId, lessonId] = location.href.match(/[0-9]+/g);
-
-    const request = new Request({ url: `courses/${courseId}/lessons/${lessonId}` });
-    const lessonJson = await request.make();
-
-    const searchQuery = lessonJson.title;
-
-    window.open(`https://www.google.com/search?q=${searchQuery}`);
-  };
-
-  searchElement.addEventListener('click', searchGoogle);
+  return searchButtonElement;
 }
 
-export default function createSearchButtonObserver() {
-  const observer = new Observer({
-    targetElementSelector: 'div[class*="theory__Root"]',
-    createdElementSelector: '.searchButton',
-    urlPart: 'courses',
-    callback: addSearchButton,
+async function searchButtonElementOnClick(e) {
+  e.preventDefault();
+  const { courseId, lessonId } = getLinkMeta();
+  const conspectData = await getConspectData({ courseId, lessonId });
+  const conspectTitle = getConspectTitle(conspectData);
+
+  openGoogleSearch(conspectTitle);
+}
+
+function getLinkMeta() {
+  const [courseId, lessonId] = location.href.match(/[0-9]+/g);
+
+  return { courseId, lessonId };
+}
+
+async function getConspectData({ courseId, lessonId }) {
+  const request = new Request({ url: `courses/${courseId}/lessons/${lessonId}` });
+  const data = await request.make();
+
+  return data;
+}
+
+function getConspectTitle(data) {
+  return data.title;
+}
+
+function openGoogleSearch(query) {
+  window.open(`https://www.google.com/search?q=${query}`);
+}
+
+function createSearchButtonIconElement(searchButtonElement) {
+  const searchButtonIconElement = new Element({
+    tag: 'img',
+    properties: { src: googleIcon },
+    parent: searchButtonElement,
   });
 
-  return observer;
+  return searchButtonIconElement;
 }
