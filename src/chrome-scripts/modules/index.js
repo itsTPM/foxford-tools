455230c7bf3b8ffa06f92491ca273ee03d70d923
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-12-29 21:40:12 +0300

    refactor(readingList)

--- src/chrome-scripts/modules/index.js
+++ src/chrome-scripts/modules/index.js
@@ -1,9 +1,6 @@
 import debounce from './debounce.js';
-import getReadingList from './getReadingList.js';
 import getSettings from './getSettings.js';
 import injectCss from './injectCss.js';
 import logger from './logger.js';
-import setReadingList from './setReadingList.js';
-import updateReadingList from './updateReadingList.js';
 
-export { debounce, getReadingList, getSettings, injectCss, logger, setReadingList, updateReadingList };
+export { debounce, getSettings, injectCss, logger };
--- src/chrome-scripts/observers/readingList.js
+++ src/chrome-scripts/observers/readingList.js
@@ -1,57 +1,110 @@
-import { Observer, Element, Request } from '../classes';
-import { getReadingList, updateReadingList } from '../modules';
-import bookmarkPlus from '../assets/bookmark-plus.svg?url';
-import bookmarkMinus from '../assets/bookmark-minus.svg?url';
-
-async function addReadingListButton(element) {
-  const readingListButton = new Element({
-    tag: 'button',
-    properties: { className: 'readingListButton' },
-    parent: element.parentNode,
-    insertMethod: 'prepend',
-  });
-
-  let readingList = await getReadingList();
-
-  const currentUrl = location.href;
-  let isAdded = readingList.some((item) => item.url === currentUrl);
-
-  const img = new Element({
-    tag: 'img',
-    properties: { src: isAdded ? bookmarkMinus : bookmarkPlus },
-    parent: readingListButton,
-  });
-
-  const toggleList = async () => {
-    isAdded = !isAdded;
-
-    const [lessonId, conspectId] = location.href.match(/[0-9]+/g);
-    const request = new Request({ url: `lessons/${lessonId}/conspects/${conspectId}` });
-    const conspectJson = await request.make();
-
-    const {
-      name: title,
-      course: { id: courseId, name: courseName },
-      discipline: { color: courseColor, image_url: courseImage },
-    } = conspectJson;
-
-    const readingListItem = { url: currentUrl, title, courseId, courseName, courseColor, courseImage };
-
-    readingList = await updateReadingList(readingList, isAdded ? readingListItem : currentUrl, isAdded);
-
-    img.src = isAdded ? bookmarkMinus : bookmarkPlus;
-  };
-
-  readingListButton.addEventListener('click', toggleList);
-}
+import { Element, Observer, Request } from '../classes';
+import bookmarkMinusIcon from '../assets/bookmark-minus.svg?url';
+import bookmarkPlusIcon from '../assets/bookmark-plus.svg?url';
 
 export default function createReadingListObserver() {
   const observer = new Observer({
     targetElementSelector: '#wikiThemeContent',
-    createdElementSelector: '.readingListButton',
+    createdElementSelector: '#readingListButton',
     urlPart: 'conspects',
-    callback: addReadingListButton,
+    callback: observerCallback,
   });
 
   return observer;
+}
+
+async function observerCallback(element) {
+  const conspectUrl = location.href;
+  let readingList = await getReadingList();
+  let isAdded = checkIsAdded({ readingList, conspectUrl });
+
+  const button = createReadingListButton(element);
+  const icon = createReadingListIcon(button);
+  icon.src = getIconSrc(isAdded);
+
+  button.addEventListener('click', toggleItemInList);
+
+  async function toggleItemInList() {
+    const [lessonId, conspectId] = getLessonAndConspectIds(conspectUrl);
+    const conspectData = await getConspectData({ lessonId, conspectId });
+
+    const readingListItem = createReadingListItem({ conspectData, conspectUrl });
+
+    isAdded = !isAdded;
+
+    readingList = await updateReadingList({ readingListItem, isAdded });
+
+    icon.src = getIconSrc(isAdded);
+  }
+}
+
+async function getReadingList() {
+  const storage = await chrome.storage.sync.get(['readingList']);
+
+  return storage.readingList || [];
+}
+
+function checkIsAdded({ readingList, conspectUrl }) {
+  return readingList.some((item) => item.url === conspectUrl);
+}
+
+function createReadingListButton(element) {
+  const readingListButton = new Element({
+    tag: 'button',
+    properties: { className: 'readingListButton', id: 'readingListButton' },
+    parent: element.parentNode,
+    insertMethod: 'prepend',
+  });
+
+  return readingListButton;
+}
+
+function createReadingListIcon(parent) {
+  const readingListIcon = new Element({
+    tag: 'img',
+    parent,
+  });
+
+  return readingListIcon;
+}
+
+function getIconSrc(isAdded) {
+  if (isAdded) {
+    return bookmarkMinusIcon;
+  }
+
+  return bookmarkPlusIcon;
+}
+
+function getLessonAndConspectIds(conspectUrl) {
+  return conspectUrl.match(/[0-9]+/g);
+}
+
+async function getConspectData({ lessonId, conspectId }) {
+  const request = new Request({ url: `lessons/${lessonId}/conspects/${conspectId}` });
+  const data = await request.make();
+
+  return data;
+}
+
+function createReadingListItem({ conspectData, conspectUrl }) {
+  const {
+    name: title,
+    course: { id: courseId, name: courseName },
+    discipline: { color: courseColor, image_url: courseImage },
+  } = conspectData;
+
+  return { url: conspectUrl, title, courseId, courseName, courseColor, courseImage };
+}
+
+async function updateReadingList({ readingListItem, isAdded }) {
+  const currentList = await getReadingList();
+  const url = readingListItem.url;
+  const updatedList = isAdded ? [...currentList, readingListItem] : currentList.filter((item) => item.url !== url);
+  await setReadingList(updatedList);
+  return updatedList;
+}
+
+async function setReadingList(readingList) {
+  await chrome.storage.sync.set({ readingList });
 }
