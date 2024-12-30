1c069816e5fa08fa524cdfb4792766d38cae39e9
itsTPM <26815728+itsTPM@users.noreply.github.com>
2024-12-30 11:54:15 +0300

    fix(tests): folder name

--- src/chrome-scripts/classes/tests/Request.test.js
+++ src/chrome-scripts/classes/tests/Request.test.js
@@ -2,7 +2,7 @@ import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { Request } from '../';
 import { logger } from '../../utils';
 
-vi.mock('../../modules', () => ({
+vi.mock('../../utils', () => ({
   logger: {
     error: vi.fn(),
   },
