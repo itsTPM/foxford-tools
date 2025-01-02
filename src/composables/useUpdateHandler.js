7f1650a350894810194847f403c99684c31cc3e7
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-01-02 13:36:52 +0300

    refactor(UpdateHandler): move logic to composable

---
+++ src/composables/useUpdateHandler.js
@@ -0,0 +1,23 @@
+export function useUpdateHandler() {
+  async function updateHandler() {
+    const storageUpdateData = await getUpdateDataFromStorage();
+
+    if (!storageUpdateData) return;
+
+    await resetUpdateDataInStorage();
+    await chrome.runtime.sendMessage('clearBadge');
+
+    return storageUpdateData;
+  }
+
+  return { updateHandler };
+}
+
+async function getUpdateDataFromStorage() {
+  const updateData = await chrome.storage.local.get('updateData');
+  return updateData.updateData;
+}
+
+async function resetUpdateDataInStorage() {
+  await chrome.storage.local.remove('updateData');
+}
