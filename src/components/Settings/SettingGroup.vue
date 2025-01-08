701abd789289c04a55d8092cd1e989bfcfc9db5c
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-01-08 23:16:36 +0300

    refactor(Settings): small improvements

--- src/components/Settings/SettingGroup.vue
+++ src/components/Settings/SettingGroup.vue
@@ -17,7 +17,8 @@ const emits = defineEmits(['selectSettingGroup']);
 
 <template>
   <Button
-    class="relative col-start-1 w-10 p-0 duration-300 after:pointer-events-none after:absolute after:left-[calc(100%+1rem)] after:z-10 after:block after:w-fit after:-translate-x-full after:rounded-md after:border after:border-border after:bg-background/90 after:p-1 after:opacity-0 after:shadow-2xl after:backdrop-blur-xl after:transition-all after:duration-500 after:content-[attr(data-title)] after:hover:translate-x-0 after:hover:opacity-100"
+    class="relative after:pointer-events-none after:absolute after:left-[calc(100%+1rem)] after:z-10 after:block after:w-fit after:-translate-x-full after:rounded-md after:border after:border-border after:bg-background/90 after:p-1 after:opacity-0 after:shadow-2xl after:backdrop-blur-xl after:transition-all after:duration-500 after:content-[attr(data-title)] after:hover:translate-x-0 after:hover:opacity-100"
+    size="icon"
     variant="outline"
     :class="{
       'bg-active': selectedSettingGroup === settingGroup,
