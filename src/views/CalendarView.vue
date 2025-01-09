d09ac965335e987f8e56038857f1cbf4d2723af2
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-01-09 10:36:57 +0300

    refactor(Calendar): make composable

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -1,38 +1,32 @@
 <script setup>
-import { onMounted, ref } from 'vue';
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import { ref } from 'vue';
 import { IconCopy, IconCheck } from '@tabler/icons-vue';
 
-let calendarLink = ref('');
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useCalendar } from '@/composables/useCalendar';
+const { calendarLink, getCalendarLink, setCalendarLink, loadSavedCalendarLink } = useCalendar();
+
 let isCopied = ref(false);
-
-onMounted(() => {
-  if (localStorage.getItem('calendarLink')) {
-    calendarLink.value = localStorage.getItem('calendarLink');
-  }
-});
-
-function getCalendarLink() {
-  fetch('https://foxford.ru/api/calendar?date_from=2077-01-01&date_to=2077-01-01')
-    .then((response) => response.json())
-    .then((data) => {
-      calendarLink.value = data.share_url;
-      localStorage.setItem('calendarLink', data.share_url);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
-}
 
 function copyToClipboard() {
   navigator.clipboard.writeText(calendarLink.value);
   isCopied.value = true;
 }
+
+async function getAndSetCalendarLink() {
+  const newCalendarLink = await getCalendarLink();
+
+  if (newCalendarLink) {
+    setCalendarLink(newCalendarLink);
+  }
+}
+
+loadSavedCalendarLink();
 </script>
 
 <template>
-  <Button class="flex w-full flex-col p-6" variant="outline" @click="getCalendarLink" v-if="!calendarLink">
+  <Button class="flex w-full flex-col p-6" variant="outline" @click="getAndSetCalendarLink" v-if="!calendarLink">
     <p>Получить ссылку на свой календарь</p>
     <p class="text-xs font-normal">она откроется и у других</p>
   </Button>
