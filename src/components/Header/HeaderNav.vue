a6d89fab95a1fdd2a63aedf32e26f8c874be4ca0
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-01-10 18:20:56 +0300

    refactor(HeaderNav): get routes from useRouter, move icons to router

--- src/components/Header/HeaderNav.vue
+++ src/components/Header/HeaderNav.vue
@@ -1,35 +1,10 @@
 <script setup>
-import { IconBookmarks, IconBrush, IconSettings, IconCalendarShare, IconUserCircle } from '@tabler/icons-vue';
+import { useRouter } from 'vue-router';
+
 import { Button } from '@/components/ui/button';
 
-// Маршруты для отображения на панели навигации
-const routes = [
-  {
-    name: 'settings',
-    friendlyName: 'Настройки',
-    icon: IconSettings,
-  },
-  {
-    name: 'themes',
-    friendlyName: 'Кастомизация',
-    icon: IconBrush,
-  },
-  {
-    name: 'bookmarks',
-    friendlyName: 'Закладки',
-    icon: IconBookmarks,
-  },
-  {
-    name: 'calendar',
-    friendlyName: 'Календарь',
-    icon: IconCalendarShare,
-  },
-  {
-    name: 'account',
-    friendlyName: 'Аккаунт',
-    icon: IconUserCircle,
-  },
-];
+const router = useRouter();
+const routes = router.getRoutes();
 </script>
 
 <template>
@@ -37,14 +12,14 @@ const routes = [
     <ul class="flex justify-center gap-3" aria-label="Вкладки">
       <li v-for="route in routes">
         <Button
-          :aria-label="route.friendlyName"
+          :aria-label="route.meta.title"
           :class="$route.name === route.name ? 'bg-active' : ''"
           size="icon"
           variant="outline"
           :key="route.name"
           as-child>
           <RouterLink :to="{ name: route.name }">
-            <component :is="route.icon" class="w-6 text-foreground" stroke-width="1.5" aria-hidden="true" />
+            <component :is="route.meta.icon" class="w-6 text-foreground" stroke-width="1.5" aria-hidden="true" />
           </RouterLink>
         </Button>
       </li>
