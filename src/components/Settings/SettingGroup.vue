efdf18825800f893e65d1623d8ca69531802ae9f
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-01-10 17:51:36 +0300

    style(views): linebreaks

--- src/components/Settings/SettingGroup.vue
+++ src/components/Settings/SettingGroup.vue
@@ -18,7 +18,7 @@ const isSettingGroupSelected = computed(() => selectedSettingGroup === props.set
 </script>
 
 <template>
-  <TooltipProvider delayDuration="0" disableHoverableContent>
+  <TooltipProvider :delayDuration="0" disableHoverableContent>
     <Tooltip>
       <TooltipTrigger asChild>
         <Button
