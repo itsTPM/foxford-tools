ea0ac0492afc38f0525c3bd08a31fb12e0460b95
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-01-11 23:53:04 +0300

    test(debounce): add short delay

--- src/chrome-scripts/utils/tests/debounce.test.js
+++ src/chrome-scripts/utils/tests/debounce.test.js
@@ -2,15 +2,29 @@ import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { debounce } from '..';
 
 const DELAY = 1000;
+const SHORT_DELAY = 1;
 
 describe('debounce', () => {
-  let func, debouncedFunc;
+  let func, debouncedFunc, quickDebouncedFunc;
 
   beforeEach(() => {
     vi.useFakeTimers();
 
     func = vi.fn();
     debouncedFunc = debounce(func, DELAY);
+    quickDebouncedFunc = debounce(func, SHORT_DELAY);
+  });
+
+  it('should work correctly with minimal delays', async () => {
+    quickDebouncedFunc();
+    quickDebouncedFunc();
+    quickDebouncedFunc();
+
+    expect(func).not.toHaveBeenCalled();
+
+    vi.advanceTimersByTime(SHORT_DELAY);
+
+    expect(func).toHaveBeenCalledTimes(1);
   });
 
   it('should delay the execution of the function', () => {
