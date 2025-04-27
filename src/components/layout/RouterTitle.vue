c3878f4d90e49833e2b39a0109f93000219c82aa
itsTPM <26815728+itsTPM@users.noreply.github.com>
2025-04-28 01:16:44 +0300

    fix(RouterTitle): use useRoute instead of this.$route

--- src/components/layout/RouterTitle.vue
+++ src/components/layout/RouterTitle.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+</script>
+
 <template>
-  <h2 class="text-center text-base font-medium">{{ this.$route.meta.title }}</h2>
+  <h2 class="text-center text-base font-medium">{{ route.meta.title }}</h2>
 </template>
