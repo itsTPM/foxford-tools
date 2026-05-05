177782ecc6236343b0c56056d2c72dbab8e6bc40
itsTPM <26815728+itsTPM@users.noreply.github.com>
2026-05-05 11:50:48 +0300

    Refactor Bookmark UI

--- src/components/Bookmarks/Bookmark.vue
+++ src/components/Bookmarks/Bookmark.vue
@@ -1,6 +1,5 @@
 <script setup>
-import { IconX } from '@tabler/icons-vue';
-
+import { IconTrash } from '@tabler/icons-vue';
 import { Button } from '../ui/button/';
 import { useBookmarks } from '@/composables/useBookmarks';
 const { removeBookmark } = useBookmarks();
@@ -12,27 +11,26 @@ const props = defineProps({
   },
 });
 
-function openBookmark() {
-  window.open(props.bookmark.url, '_blank');
+function handleRemoveBookmark() {
+  removeBookmark(props.bookmark);
 }
 </script>
 
 <template>
-  <Button variant="outline" class="relative grid h-fit grid-cols-6 px-3 font-normal text-balance" @click="openBookmark">
-    <div class="col-span-4 flex flex-col gap-1 text-left whitespace-normal">
-      <p class="text-base leading-5">{{ bookmark.title }}</p>
-      <p class="text-muted-foreground text-xs">{{ bookmark.courseName }}</p>
-    </div>
-    <div class="col-span-2 flex items-center justify-end">
-      <img alt="" :src="bookmark.courseImage" class="max-h-14" />
-    </div>
-    <div class="absolute top-1 right-1">
-      <button
-        class="text-muted-foreground focus-within:ring-ring hover:text-secondary-foreground transition focus-within:ring-2 focus:outline-hidden"
-        aria-label="Удалить закладку"
-        @click.stop="removeBookmark(bookmark)">
-        <IconX class="size-5" aria-hidden="true" />
-      </button>
-    </div>
-  </Button>
+  <div class="flex">
+    <Button class="h-auto w-auto border-r-0 p-2" variant="outline" @click="handleRemoveBookmark">
+      <IconTrash stroke-width="1.5" class="size-5" aria-label="Удалить" />
+    </Button>
+
+    <Button variant="outline" class="h-auto flex-1 justify-between gap-2 px-2 py-1 whitespace-normal" as-child>
+      <a :href="bookmark.url" target="_blank">
+        <div>
+          <p class="text-sm">{{ bookmark.title }}</p>
+          <p class="text-muted-foreground font-normal">{{ bookmark.courseName }}</p>
+        </div>
+
+        <img alt="" :src="bookmark.courseImage" class="aspect-square w-16 object-contain" />
+      </a>
+    </Button>
+  </div>
 </template>
