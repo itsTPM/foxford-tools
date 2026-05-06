f9ac96343e52b621a3b57a534312f7f4ea512ee4
itsTPM <26815728+itsTPM@users.noreply.github.com>
2026-05-06 12:03:36 +0300

    Init TypeScript

---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist/**']),
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.webextensions,
+        ...globals.jest,
+        global: false,
+      },
+    },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+]);
