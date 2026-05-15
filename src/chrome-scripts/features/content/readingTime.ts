7dcc98b812caa24c307681b581d7b959e6d38b67
itsTPM <26815728+itsTPM@users.noreply.github.com>
2026-05-15 08:26:26 +0300

    Add tests for chrome-scripts/features/content/readingTime

--- src/chrome-scripts/features/content/readingTime.ts
+++ src/chrome-scripts/features/content/readingTime.ts
@@ -27,17 +27,17 @@ function getConspectText(element: Element) {
   return element.textContent ?? '';
 }
 
-function calculateWordCount(text: string) {
+export function calculateWordCount(text: string) {
   return [...text.matchAll(/[^\s]+/g)].length;
 }
 
-function calculateReadingTime(wordCount: number) {
+export function calculateReadingTime(wordCount: number) {
   const WORDS_PER_MINUTE = 150;
 
   return Math.round(wordCount / WORDS_PER_MINUTE);
 }
 
-function calculateReadingTimeElementText(readingTime: number) {
+export function calculateReadingTimeElementText(readingTime: number) {
   if (readingTime > 0) {
     return `~${readingTime} мин. чтения`;
   }
